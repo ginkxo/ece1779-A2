120f713ed468eb90e371babf191bc5ec1cedaf67
andrewmbrown <17amb@queensu.ca>
2021-11-16 00:07:38 -0500

    updated workers code

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+# a random key for interacting with flask dictionaries - protects cookie tampering
+app.config['SECRET_KEY'] = "a8e23cgqg45iy20120e19e2954796e8f"
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from app import app
+from datetime import datetime, timedelta
 import boto3
 import sys
 
@@ -15,14 +16,102 @@ def workers():
     ec2 = boto3.resource('ec2')
 
     # find all instances with a filter name and running
-    instances = ec2.instances.filter(
+    ec2_instances = ec2.instances.filter(
         Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 
     # low level client to interact with cloud watch
     # tracks metrics for aws resources
     client = boto3.client('cloudwatch')
-    metric_name = 'CPUUtilization'
-    statistic = 'Average'
+    metric_name = 'CPU_Utilization'  # cloudwatch monitoring CPU
+    stats = 'Average'
 
+    # dictionaries to store data
+    CPU_Util = {}
+    HTTP_Req = {}
+
+    # loop through each instance to display metrics
+    for instance in ec2_instances:
+
+        # HTTP req metric
+        time_stamps = []        
+        requests = []
+        # using cloudwatch, get metrics from ec2 instance within a window of time
+        """
+        response, dictionary containing metrics
+        Period: 60s
+        StartTime: Start monitoring
+        Endtime: Stop monitoring
+        MetricName: name of this measurement
+        Namespace: HTTP request name
+        Statistics: Maximum value from single observation
+        Dimensions: specific instance specified
+        """
+        response = client.get_metric_statistics(
+            Period=1 * 60,
+            StartTime=datetime.utcnow() - timedelta(seconds=30 * 60),
+            EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
+            MetricName='HTTP_Requests',
+            Namespace='HTTP_Requests',
+            Statistics=['Maximum'],
+            Dimensions=[
+                    {
+                    'Name': 'Instance_ID',
+                    'Value': instance.id
+                    },
+                ]
+        )
+        print('This is the custom metric:')
+
+        requests = []
+        time_stamps = []
+        for point in response["Datapoints"]:
+            hour = point['Timestamp'].hour
+            minute = point['Timestamp'].minute
+            time = hour + minute/60
+            time_stamps.append(round(time, 2))
+            requests.append(point['Maximum'])
+        
+        indexes = list(range(len(time_stamps)))
+        indexes.sort(key=time_stamps.__getitem__)
+        time_stamps = list(map(time_stamps.__getitem__, indexes))
+        requests = list(map(requests.__getitem__, indexes))
+        HTTP_Req['localhost'] = [time_stamps, requests]
+        for i in range(len(time_stamps)):
+            print(time_stamps[i], requests[i])
+
+        # CPU Util metrics
+        time_stamps = []
+        cpu_stats = []
+        response = client.get_metric_statistics(
+            Period=1 * 60,
+            StartTime=datetime.utcnow() - timedelta(seconds=30 * 60),
+            EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
+            MetricName=metric_name,
+            Namespace='AWS/EC2',
+            Statistics=[stats],
+            Dimensions=[{'Name': 'InstanceId', 'Value': instance.id}]
+        )
+
+        for point in response['Datapoints']:
+            hour = point['Timestamp'].hour
+            minute = point['Timestamp'].minute
+            time = hour + minute/60
+            time_stamps.append(round(time, 2))
+            cpu_stats.append(round(point['Average'], 2))
+        indexes = list(range(len(time_stamps)))
+        indexes.sort(key=time_stamps.__getitem__)
+        time_stamps = list(map(time_stamps.__getitem__, indexes))
+        cpu_stats = list(map(cpu_stats.__getitem__, indexes))
+        CPU_Util[instance.id] = [time_stamps, cpu_stats]
     
+    # CPU_Util is a dictionary, with keys = instance_id, and values = [sorted time_stamps, cpu_utilization values]
+
+    #for key, value in CPU_Util.items():
+    #    print('Labels: ', value[0])
+    #   print('CPU Utilization: ', value[1])
+
+    if not CPU_Util:
+        flash('Worker pool currently empty, please manually start some instances.', category='danger')
+    else:
+        flash('There are currently {} worker(s).'.format(len(CPU_Util)), category='success')
     return render_template('workers.html')
