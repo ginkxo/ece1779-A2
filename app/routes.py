fa2393931acf4dfe560cde5e55d92bde3aed0756
andrewmbrown <17amb@queensu.ca>
2021-11-15 20:03:06 -0500

    workers functionality

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for, flash, redirect, request
 from app import app
 import boto3
+import sys
 
 #Default route and login route the same
 @app.route('/')
@@ -10,5 +11,18 @@ def index():
 
 @app.route('/workers')
 def workers():
+    # creates a connection to aws services for ec2
+    ec2 = boto3.resource('ec2')
+
+    # find all instances with a filter name and running
+    instances = ec2.instances.filter(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+
+    # low level client to interact with cloud watch
+    # tracks metrics for aws resources
+    client = boto3.client('cloudwatch')
+    metric_name = 'CPUUtilization'
+    statistic = 'Average'
+
     
-    pass
+    return render_template('workers.html')
